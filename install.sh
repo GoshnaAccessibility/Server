87bbd17f2a6ceba898067a68734015576e5fc738
Christopher Bull <CaffeinatedAndroid@users.noreply.github.com>
2018-11-30 14:58:36 +0000

    Shell scripts tweaked
    Shell scripts updated, so they function nicely with systemd. Also renamed a README as a markdown file.

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash -
 sudo apt-get install python-virtualenv
 rm -rf flask/
 virtualenv flask
--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+#!/bin/bash -
 source flask/bin/activate
 FLASK_APP=app.py flask run --host=0.0.0.0
 #PYTHONPATH=~/Git/Goshna-Server/ ./app.py
